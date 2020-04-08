39b39b247744f7394dd72270ef88180ecdb6522f
Gabriel Engel <gfbengel@gmail.com>
2020-04-08 19:50:17 -0300

    testes parciais

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -21,7 +21,7 @@ routes.post('/ongs', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email(),
-    whatsapp: Joi.number().required().min(10).max(11),
+    whatsapp: Joi.string().required().min(10).max(11),
     city: Joi.string().required(),
     uf: Joi.string().required().length(2),
   })
@@ -41,7 +41,7 @@ routes.post('/incidents', celebrate({
   [Segments.BODY]: Joi.object().keys({
     title: Joi.string().required(),
     description: Joi.string().required(),
-    value: Joi.number().required()
+    value: Joi.number().required().min(0)
   })
 }), IncidentController.create)
 
