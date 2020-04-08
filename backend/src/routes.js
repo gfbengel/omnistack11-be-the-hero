fd1acadf73cd1310d0e7121c187ec6e832003280
Gabriel Engel <gfbengel@gmail.com>
2020-04-08 19:11:38 -0300

    validação do login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,7 +9,11 @@ const SessionController = require('./controllers/SessionController')
 const routes = express.Router()
 
 
-routes.post('/sessions', SessionController.create)
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required(), 
+  })
+}), SessionController.create)
 
 routes.get('/ongs', OngController.index)
 
